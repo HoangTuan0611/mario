aae774890dfdc0bbf6c4b27dd3cff2cc5cb033b8
HoangTuan0611 <48887111+HoangTuan0611@users.noreply.github.com>
2022-05-18 23:37:27 +0700

    define value goomba

--- 05-SceneManager/Goomba.h
+++ 05-SceneManager/Goomba.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "GameObject.h"
 
-#define GOOMBA_GRAVITY 0.002f
-#define GOOMBA_WALKING_SPEED 0.05f
+#define GOOMBA_WALKING_SPEED		0.035f
+#define GOOMBA_GRAVITY				0.002f
+#define GOOMBA_DIE_DEFLECT_SPEED	0.35f
+#define GOOMBA_JUMP_SPEED			0.125f
+#define GOOMBA_HIGHJUMP_SPEED		0.25f
 
 #define GOOMBA_ADD_Y 8
 
@@ -18,13 +21,20 @@
 #define ID_ANI_GOOMBA_WALKING 5000
 #define ID_ANI_GOOMBA_DIE 5001
 
+#define GOOMBA_NORMAL			0
+#define GOOMBA_RED				1
+#define GOOMBA_RED_NORMAL		2
+
 class CGoomba : public CGameObject
 {
 protected:
 	float ax;				
 	float ay; 
+	int jumpingStacks = 0;
 
 	ULONGLONG die_start;
+	ULONGLONG chasingTimer;
+	ULONGLONG walkingTimer;
 
 	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects);
