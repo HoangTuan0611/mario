1be83c792fa30bcff8b66619e067df2776195aa4
HoangTuan0611 <48887111+HoangTuan0611@users.noreply.github.com>
2022-05-24 14:16:49 +0700

    update Coin object, setState for Coin

--- 05-SceneManager/Coin.h
+++ 05-SceneManager/Coin.h
@@ -5,17 +5,30 @@
 
 #define ID_ANI_COIN 11000
 
+#define COIN_STATE_IDLE		0
+
 #define	COIN_WIDTH 10
 #define COIN_BBOX_WIDTH 10
 #define COIN_BBOX_HEIGHT 16
 
+#define COIN_TYPE_NORMAL	6
 #define COIN_TYPE_INBRICK	61
 
 class CCoin : public CGameObject {
+	bool isAppear;
+
 public:
 	CCoin(float x, float y) : CGameObject(x, y) {}
+	CCoin(int type = COIN_TYPE_NORMAL);
+
 	void Render();
 	void Update(DWORD dt) {}
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	int IsBlocking() { return 0; }
+
+	void SetAppear(bool appear) { isAppear = appear; }
+	void SetState(int state);
+
+	bool GetAppear() { return isAppear; }
+
 };
