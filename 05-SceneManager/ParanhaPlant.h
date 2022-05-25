fee0e425e792c8cb112dff756062a64ebee41ab6
HoangTuan0611 <48887111+HoangTuan0611@users.noreply.github.com>
2022-05-25 16:49:42 +0700

    init piranhaPlant object

---
+++ 05-SceneManager/ParanhaPlant.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "GameObject.h"
+#include "Utils.h"
+#include "debug.h"
+
+#define PIRANHAPLANT_BBOX_WIDTH					20
+#define PIRANHAPLANT_BBOX_HEIGHT				24
+
+#define PIRANHAPLANT_ANI_IDLE	0
+
+class PiranhaPlant :
+	public CGameObject
+{
+
+public:
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* colliable_objects = NULL);
+	virtual void Render();
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void SetState(int state);
+
+	PiranhaPlant();
+};
