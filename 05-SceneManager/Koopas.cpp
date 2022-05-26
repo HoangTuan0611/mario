8904d1e97141cc2f682be23b702ee2d87d1988d7
HoangTuan0611 <48887111+HoangTuan0611@users.noreply.github.com>
2022-05-26 16:07:45 +0700

    init koopas object

---
+++ 05-SceneManager/Koopas.cpp
@@ -0,0 +1,57 @@
+#include "Koopas.h"
+
+CKoopas::CKoopas(int tag)
+{
+	this->start_x = x;
+	this->start_y = y;
+	this->nx = -1;
+	this->SetState(KOOPAS_STATE_WALKING);
+}
+
+void CKoopas::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
+	
+	vy += ay * dt;
+	vx += ax * dt;
+
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
+void CKoopas::OnNoCollision(DWORD dt) {
+	x += vx * dt;
+	y += vy * dt;
+}
+
+void CKoopas::OnCollisionWith(LPCOLLISIONEVENT e) {
+	if (e->ny != 0 && e->obj->IsBlocking())
+	{
+		vy = 0;
+	}
+}
+
+void CKoopas::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+	left = x;
+	top = y;
+	right = x + KOOPAS_BBOX_WIDTH;
+	bottom = y + KOOPAS_BBOX_HEIGHT;
+}
+
+void CKoopas::Render()
+{
+	int ani = -1;
+
+	animation_set->at(ani)->Render(x, y);
+	RenderBoundingBox();
+}
+
+void CKoopas::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case KOOPAS_STATE_WALKING:
+		vx = this->nx * KOOPAS_WALKING_SPEED;
+		break;
+	}
+}
---
+++ 05-SceneManager/Koopas.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "GameObject.h"
+
+#define KOOPAS_BBOX_WIDTH				16
+#define KOOPAS_BBOX_HEIGHT				20
+
+#define KOOPAS_STATE_WALKING	100
+
+#define KOOPAS_WALKING_SPEED			0.03f
+
+#define KOOPAS_ANI_WALKING_RIGHT	0
+
+class CKoopas : public CGameObject
+{
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	virtual void Render();
+	float ax;
+	float ay;
+public:
+
+	CKoopas(int tag);
+
+	virtual void SetState(int state);
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 0; }
+
+	virtual void OnNoCollision(DWORD dt);
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+
+};
