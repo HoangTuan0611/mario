a0027201325f207eac2d4cd559d43b0de41032de
HoangTuan0611 <48887111+HoangTuan0611@users.noreply.github.com>
2022-06-19 22:21:18 +0700

    update render for piranhaPlantFire and set limit Y

--- 05-SceneManager/PiranhaPlantFire.h
+++ 05-SceneManager/PiranhaPlantFire.h
@@ -7,19 +7,32 @@
 #define PIRANHAPLANT_DARTING_SPEED				0.03f
 #define PIRANHAPLANT_STATE_DARTING				0
 
+#define PIRANHAPLANT_GREEN_BBOX_HEIGHT			24
+#define PIRANHAPLANT_RED_BBOX_HEIGHT			32
+
 #define BB_Height	0
+
+#define PIRANHAPLANT_ANI_RIGHT_UP			0
+#define PIRANHAPLANT_ANI_RIGHT_DOWN			1
+#define PIRANHAPLANT_ANI_LEFT_UP			2
+#define PIRANHAPLANT_ANI_LEFT_DOWN			3
 
 
 class PiranhaPlantFire :
 	public PiranhaPlant
 {
 	float limitY = 0;
+	int BBHeight = 0;
+
+	bool Up = false;
+	bool Right = false;
 
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* colliable_objects = NULL);
 	virtual void Render();
 public:
 
 	PiranhaPlantFire(int tag);
+
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void SetState(int state);
 
@@ -28,7 +41,10 @@ public:
 
 	void SetLimitY(float ly)
 	{
-		limitY = ly - PIRANHAPLANT_BOX_HEIGHT;
+		if (tag == 0)
+			BBHeight = PIRANHAPLANT_GREEN_BBOX_HEIGHT;
+		else BBHeight = PIRANHAPLANT_RED_BBOX_HEIGHT;
+		limitY = ly - BBHeight;
 	}
 
 };
